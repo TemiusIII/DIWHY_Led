ea83e7829a8849c6d4b6f528a80d63cf7cccbcad
TemiusIII <artem.smirnov2005@mail.ru>
2024-10-20 18:55:37 +0300

    ADD: Ripple effect(works bad on esp8266)

--- esp32.ino
+++ esp32.ino
@@ -3,22 +3,45 @@
 #include <WiFiClient.h>
 #include <FastLED.h>
 
+#define NUM_LEDS 865 // Amount of LEDs in strip
+#define PIN_LED 15     // Digital pin
+#define LED_BRIGHTNESS 100
+#define RIPPLE_SPLIT 4 // how much you want to split ripple effect show(its too heavy for big strips) 0 to not split
 
-#define NUM_LEDS 899 // Amount of LEDs in strip
-#define PIN_LED 5 // Digital pin
+
+#define SERVER_URL actual_server_url
+
+
 CRGB leds[NUM_LEDS];
 
-const char* ssid = "YOUR SSID";
-const char* password = "YOUR PASSWORD";
+const char *ssid = "YOUR_SSID";
+const char *password = "YOUR_PASSWORD";
+// some structs for fun
+struct hsv_color{
+    int hue;
+    int saturation;
+    int value;
+};
+
+struct ripple_params{
+    int hue_gap;
+    int hue_range;
+    int ripple_step;
+};
 
 // Your Domain name with URL path or IP address with path
-int led_mode = 2;
-int speed = 30;
+int counter = 0;
+int led_mode = 0;
+int led_speed = 30;
 long color = 0xFFFFFF;
-String led_link = "http://SERVER_URL/led_mode"; // SERVER_URL looks like 192.168.1.21:8080
-String color_link = "http://SERVER_URL/color";
-String speed_link = "http://SERVER_URL/speed";
+String led_link = String("http://") + SERVER_URL + "/led_mode";
+String color_link = String("http://") + SERVER_URL + "/color";
+String color_hsv_link = String("http://") + SERVER_URL + "/color_hsv";
+String led_speed_link = String("http://") + SERVER_URL + "/speed";
+String ripple_val_link = String("http://") + SERVER_URL + "/ripple_values";
 
+hsv_color color_hsv = {0, 0, 100};
+ripple_params ripple_settings = {0, 0, 0};
 
 void white(){
   for (int j = 0; j < 2; ++j){
@@ -27,7 +50,7 @@ void white(){
       }
 
       FastLED.show();
-      delay(30);
+      delay(led_speed);
   }
 
 
@@ -40,7 +63,7 @@ void black(){
     }
 
     FastLED.show();
-    delay(30);
+    delay(led_speed);
   }
 
 }
@@ -51,7 +74,7 @@ void fill_color(){
       leds[i] = color;
       }
       FastLED.show();
-      delay(30);
+      delay(led_speed);
     }
 }
 
@@ -63,7 +86,7 @@ void strab(){
 
   }
   FastLED.show();
-  delay(speed);
+  delay(led_speed);
 
 
   for (int j = 0; j < NUM_LEDS; ++j){
@@ -71,45 +94,44 @@ void strab(){
 
   }
   FastLED.show();
-  delay(speed);
+  delay(led_speed);
   }
 }
 
 
-void new_year(int wait = speed, int amount = 10){
+void new_year(int wait = led_speed, int amount = 10) {
 
-CRGB colors[2] = {CRGB::Red,CRGB::White};
+    CRGB colors[2] = {CRGB::Red,CRGB::White};
 
 
 
-int cnt = 0;
-bool red = 1;
+    int cnt = 0;
+    bool red = 1;
 
-for (int i = 0; i < amount; ++i){
-  for (int j = 0; j < NUM_LEDS; ++j){
-    leds[j] = colors[1];
-  }
-  for (int j = i; j < NUM_LEDS; ++j){
-      if (red){
-        leds[j] = colors[0];
-      } else {
+    for (int i = 0; i < amount; ++i){
+      for (int j = 0; j < NUM_LEDS; ++j){
         leds[j] = colors[1];
       }
+      for (int j = i; j < NUM_LEDS; ++j){
+          if (red){
+            leds[j] = colors[0];
+          } else {
+            leds[j] = colors[1];
+          }
 
-      cnt += 1;
+          cnt += 1;
 
-      if (cnt == amount){
-        cnt = 0;
-        red = !red;
+          if (cnt == amount){
+            cnt = 0;
+            red = !red;
+          }
+        }
+        FastLED.show();
+          delay(wait);
       }
-    }
-    FastLED.show();
-      delay(wait);
-  }
-
 }
 
-void rainbow(int wait = speed, int BRIGHTNESS = 255 , int SATURATION = 255) {
+void rainbow(int wait = led_speed, int BRIGHTNESS = 255 , int SATURATION = 255) {
   for (int j = 0; j < 255; j++) {
     for (int i = 0; i < NUM_LEDS; i++) {
       leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS);
@@ -119,6 +141,44 @@ void rainbow(int wait = speed, int BRIGHTNESS = 255 , int SATURATION = 255) {
   }
 }
 
+void rippleTick() {
+    int temp;
+    if (RIPPLE_SPLIT) {
+        for (int k = 0; k < RIPPLE_SPLIT; ++k) {
+          for(int i = k * int(NUM_LEDS / RIPPLE_SPLIT); i < (k+1) * int(NUM_LEDS / RIPPLE_SPLIT); i++) {
+
+            temp = inoise8(i * ripple_settings.ripple_step, counter);
+            leds[i] = CHSV(
+                 map(color_hsv.hue, 0, 360, 0, 255) + map(temp, 20, 60, 0, ripple_settings.hue_gap), // H
+                 constrain(map(temp, 20, 60, map(min(100, color_hsv.saturation + ripple_settings.hue_range), 0, 100, 0, 255),
+                  map(max(0, color_hsv.saturation - ripple_settings.hue_range), 0, 100, 0, 255)), 0, 255), // S
+                 constrain(map(temp, 20, 60, map(max(0, color_hsv.value - ripple_settings.hue_range), 0, 100, 0, 255),
+                  map(min(100, color_hsv.value + ripple_settings.hue_range), 0, 100, 0, 255)), 0, 255)  // V
+               );
+            counter += 20;
+
+          }
+            FastLED.show();
+        }
+    } else {
+        for(int i = 0; i < NUM_LEDS; i++) {
+
+            temp = inoise8(i * ripple_settings.ripple_step, counter);
+            leds[i] = CHSV(
+                 map(color_hsv.hue, 0, 360, 0, 255) + map(temp, 20, 60, 0, ripple_settings.hue_gap), // H
+                 constrain(map(temp, 20, 60, map(min(100, color_hsv.saturation + ripple_settings.hue_range), 0, 100, 0, 255),
+                  map(max(0, color_hsv.saturation - ripple_settings.hue_range), 0, 100, 0, 255)), 0, 255), // S
+                 constrain(map(temp, 20, 60, map(max(0, color_hsv.value - ripple_settings.hue_range), 0, 100, 0, 255),
+                  map(min(100, color_hsv.value + ripple_settings.hue_range), 0, 100, 0, 255)), 0, 255)  // V
+               );
+            counter += 20;
+
+        }
+            FastLED.show();
+    }
+
+    //  delay(led_speed);
+}
 
 
 String get_data(String link){
@@ -126,12 +186,11 @@ if(WiFi.status()== WL_CONNECTED){
   WiFiClient client;
   HTTPClient http;
 
-  String serverPath = link;
-  http.begin(client, serverPath.c_str());
+  http.begin(client, link.c_str());
 
   int httpResponseCode = http.GET();
 
-  if (httpResponseCode>0) {
+  if (httpResponseCode > 0) {
     String payload = http.getString();
     return(payload);
   } else {
@@ -148,12 +207,66 @@ if(WiFi.status()== WL_CONNECTED){
     return "0xFFFFFF";
 }
 
+void update_hsv(){
+    String temp = get_data(color_hsv_link);
+    // Serial.println("HSV raw data " + temp);
+    int space1 = temp.indexOf(' ');
+    if (space1 == -1) {
+        Serial.println("Error while fetching hue color");
+        return;
+    }
 
+    int space2(temp.indexOf(' ', space1 + 1));
+    if (space2 == -1) {
+        Serial.println("Error while fetching hue color");
+        return;
+    }
+
+    color_hsv.hue = temp.substring(0, space1).toInt();
+    color_hsv.saturation = temp.substring(space1 + 1, space2).toInt();
+    color_hsv.value = temp.substring(space2 + 1).toInt();
+}
+
+void update_ripple_settings(){
+    String temp = get_data(ripple_val_link);
+    // Serial.println("Ripple settings raw data " + temp);
+    int space1 = temp.indexOf(' ');
+    if (space1 == -1) {
+        Serial.println("Error while fetching ripple settings");
+        return;
+    }
+
+    int space2(temp.indexOf(' ', space1 + 1));
+    if (space2 == -1) {
+        Serial.println("Error while fetching ripple settings");
+        return;
+    }
+
+    ripple_settings.hue_gap = temp.substring(0, space1).toInt();
+    ripple_settings.hue_range = temp.substring(space1 + 1, space2).toInt();
+    ripple_settings.ripple_step = temp.substring(space2 + 1).toInt();
+
+}
+
+
+
+void update_variables() {
+  led_speed = get_data(led_speed_link).toInt();
+  led_mode = get_data(led_link).toInt();
+
+   if (led_mode  == 5 || led_mode == 6)
+    color = strtol(get_data(color_link).c_str(), NULL, 16);
+
+   if (led_mode == 6) {
+    update_hsv();
+    update_ripple_settings();
+   }
+ }
 
 void setup() {
   Serial.begin(115200);
   FastLED.addLeds<NEOPIXEL, PIN_LED>(leds, NUM_LEDS);
-  FastLED.setBrightness(100);
+  FastLED.setBrightness(LED_BRIGHTNESS);
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -168,37 +281,36 @@ void setup() {
 
 }
 
-void update_variables(){
-  speed = get_data(speed_link).toInt();
-  led_mode = get_data(led_link).toInt();
-  color = strtol(get_data(color_link).c_str(), NULL, 16);
-}
-
 void loop() {
+  update_variables();
 
-update_variables();
-switch(led_mode){
-  case 0:
-    black();
-    break;
-  case 1:
-    white();
-    break;
-  case 2:
-    new_year();
-    break;
-  case 3:
-    rainbow();
-    break;
-  case 4:
-    strab();
-    break;
-  case 5:
-    fill_color();
-    break;
-
-}
+  switch(led_mode){
+    case 0:
+      black();
+      break;
+    case 1:
+      white();
+      break;
+    case 2:
+      new_year();
+      break;
+    case 3:
+      rainbow();
+      break;
+    case 4:
+      strab();
+      break;
+    case 5:
+      fill_color();
+      break;
+    case 6:
+      rippleTick();
+      break;
+  }
 // Serial.println(led_mode);
-// delay(100); For debug
-
+//delay(200); // For debug
+//Serial.println(color);
+//Serial.println(String(color_hsv.hue) + "   " + String(color_hsv.saturation) + "   " + String(color_hsv.value));
+//Serial.println(String(ripple_settings.hue_gap) + "   " + String(ripple_settings.hue_range) + "   " + String(ripple_settings.ripple_step));
+//Serial.println("\n\n\n\n\n");
 }
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import flask
 from flask import Flask, render_template, redirect, request
+import colorsys
 
 app = Flask(__name__)
 
 led_mode = 0
 speed = 30
 custom_color = '#FFFFFF'
+sliders = {
+    'hue_gap': 10,
+    'hue_range': 5,
+    'ripple_step': 15
+}
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", led_mode=led_mode, custom_color=custom_color, speed=speed)
+    return render_template("index.html", led_mode=led_mode, custom_color=custom_color, speed=speed,
+                           hue_gap=sliders['hue_gap'], hue_range=sliders['hue_range'],
+                           ripple_step=sliders['ripple_step'])
 
 
 @app.route("/led_mode", methods=["GET", "POST"])
@@ -37,7 +46,7 @@ def set_mode(new_mode):
 @app.route("/set_speed", methods=["GET", "POST"])
 def set_speed():
     global speed
-    new_speed = request.form['title']
+    new_speed = request.form['speed']
 
     if new_speed.isdigit() and (0 <= int(new_speed) <= 1000):
         speed = new_speed
@@ -48,7 +57,31 @@ def set_speed():
 def set_color():
     global custom_color
     custom_color = request.args.get('color')
-    return "i'm gay"
+    return "200"
+
+
+@app.route('/update_slider', methods=['POST'])
+def update_slider():
+    data = request.json
+    sliders[data['slider_name']] = data['slider_value']
+    print(data['slider_name'], " = ", sliders[data['slider_name']])
+    return "200"
+
+
+@app.route('/color_hsv', methods=["GET", "POST"])
+def color_hsv():
+    r, g, b = int(custom_color[1:3], 16), int(custom_color[3:5], 16), int(custom_color[5:7], 16)
+    hue_color = list(colorsys.rgb_to_hsv(r / 255, g / 255, b / 255))
+    hue_color[0] = int(hue_color[0] * 360)
+    hue_color[1] = int(hue_color[1] * 100)
+    hue_color[2] = int(hue_color[2] * 100)
+    hue_color = " ".join([str(x) for x in hue_color])
+    return render_template("color_hsv.html", color_hsv=hue_color)
+
+@app.route('/ripple_values', methods=["GET", "POST"])
+def ripple_values():
+    return render_template("ripple_params.html", hue_gap=sliders['hue_gap'], hue_range=sliders['hue_range'],
+                           ripple_step=sliders['ripple_step'])
 
 
 if __name__ == "__main__":
