a7e447108ef3a538c87545ee2d6b566bd092ab4f
ml_loser <average_DL_enjoyer@proton.me>
2026-04-15 14:05:13 +0300

    Changed http requests spam to websocket + esp code optimisation

--- main.py
+++ main.py
@@ -1,103 +1,105 @@
-import flask
-from flask import Flask, render_template, redirect, request, jsonify
-import colorsys
-import requests
+from flask import Flask, render_template, request, jsonify
+from flask_sock import Sock
+import json
 
 app = Flask(__name__)
+sock = Sock(app)
 
-# Глобальные переменные для управления LED
-led_mode = 0
-speed = 30
-custom_color = '#FFFFFF'
-sliders = {
-    'hue_gap': 10,
-    'hue_range': 5,
-    'ripple_step': 15
+connected_clients = set()
+
+STATE = { # values for esp
+    "led_mode": 0,
+    "speed": 30,
+    "custom_color": 0xFFFFFF,
+    "ripple": {
+        'hue_gap': 10,
+        'hue_range': 5,
+        'ripple_step': 15
+    }
 }
 
 @app.route("/", methods=["GET"])
 def index():
-    """
-    Главная страница, где всё управление происходит на одной странице.
-    Передаём текущие значения, чтобы при загрузке можно было инициализировать элементы.
-    """
     return render_template("index.html",
-                           led_mode=led_mode,
-                           custom_color=custom_color,
-                           speed=speed,
-                           hue_gap=sliders['hue_gap'],
-                           hue_range=sliders['hue_range'],
-                           ripple_step=sliders['ripple_step'])
-
-@app.route("/led_mode", methods=["GET", "POST"])
-def check_mode():
-    return render_template("check_esp.html", led_mode=led_mode)
-
-
-@app.route("/color", methods=["GET", "POST"])
-def color_mode():
-    return render_template("color_esp.html", custom_color=custom_color[1::].upper())
-
-
-@app.route("/speed", methods=["GET", "POST"])
-def speed_mode():
-    return render_template("speed_esp.html", speed=speed)
-
-@app.route('/ripple_values', methods=["GET", "POST"])
-def ripple_values():
-    return render_template("ripple_params_esp.html", hue_gap=sliders['hue_gap'], hue_range=sliders['hue_range'],
-                           ripple_step=sliders['ripple_step'])
+                           led_mode=STATE['led_mode'],
+                           custom_color=STATE["custom_color"],
+                           speed=STATE["speed"],
+                           hue_gap=STATE['ripple']['hue_gap'],
+                           hue_range=STATE['ripple']['hue_range'],
+                           ripple_step=STATE['ripple']['ripple_step'])
 
 
 @app.route("/set_mode/<int:new_mode>", methods=["POST"])
 def set_mode(new_mode):
-    global led_mode
-    led_mode = new_mode
-    try:
-        if led_mode == 0:
-            requests.post("http://192.168.2.200:80/set_sleep/1")
-        else:
-            requests.post("http://192.168.2.200:80/set_sleep/0")
-    except Exception as e:
-        print("Couldn't set sleep mode ", e)
-    return jsonify({"status": "ok", "led_mode": led_mode})
+    global STATE
+    STATE["led_mode"] = new_mode
+    send_to_all()
+    return jsonify({"status": "ok", "led_mode": STATE["led_mode"]})
 
 @app.route("/set_speed", methods=["POST"])
 def set_speed():
-    global speed
+    global STATE
     new_speed = request.json.get('speed', None)
     if new_speed is not None and str(new_speed).isdigit():
         new_speed = int(new_speed)
         if 0 <= new_speed <= 1000:
-            speed = new_speed
-    return jsonify({"status": "ok", "speed": speed})
+            STATE['speed'] = new_speed
+            send_to_all()
+    return jsonify({"status": "ok", "speed": STATE['speed']})
 
 @app.route("/set_color", methods=["POST"])
 def set_color():
-    global custom_color
+    global STATE
     color = request.json.get('color', '#FFFFFF')
     if color.startswith('#') and len(color) == 7:
-        custom_color = color
-    return jsonify({"status": "ok", "custom_color": custom_color})
+        STATE['custom_color'] = int(color[1:], 16)
+        send_to_all()
+    print(f"NIGGA {color} GIGA {STATE['custom_color']}")
+    return jsonify({"status": "ok", "custom_color": STATE['custom_color']})
 
 @app.route("/update_slider", methods=["POST"])
 def update_slider():
+    global STATE
     data = request.json
     slider_name = data.get('slider_name')
     slider_value = data.get('slider_value')
-    if slider_name in sliders:
-        sliders[slider_name] = slider_value
-    return jsonify({"status": "ok", slider_name: sliders[slider_name]})
+    if slider_name in STATE['ripple']:
+        STATE['ripple'][slider_name] = slider_value
+        send_to_all()
+    return jsonify({"status": "ok", slider_name: slider_name})
 
-@app.route('/color_hsv', methods=["GET", "POST"])
-def color_hsv():
-    r, g, b = int(custom_color[1:3], 16), int(custom_color[3:5], 16), int(custom_color[5:7], 16)
-    hue_color = list(colorsys.rgb_to_hsv(r / 255, g / 255, b / 255))
-    hue_color[0] = int(hue_color[0] * 360)
-    hue_color[1] = int(hue_color[1] * 100)
-    hue_color[2] = int(hue_color[2] * 100)
-    hue_color = " ".join([str(x) for x in hue_color])
-    return render_template("color_hsv_esp.html", color_hsv=hue_color)
+
+@sock.route('/ws') # websocket url
+def websocket(ws):
+    connected_clients.add(ws)
+    try:
+        ws.send(json.dumps(STATE))
+        while True:
+            message = ws.receive()
+            if message:
+                print("From ESP:", message)
+    except Exception as e:
+        pass
+    finally:
+        connected_clients.discard(ws)
+
+
+def send_to_all(): # send updated values to all websocket clients
+    if not connected_clients:
+        return
+
+    msg = json.dumps(STATE)
+    dead_clients = []
+
+    for client in connected_clients:
+        try:
+            client.send(msg)
+        except:
+            dead_clients.append(client)
+
+    for dead in dead_clients:
+        connected_clients.discard(dead)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
---
+++ platformio/esp32/src/main.cpp
@@ -0,0 +1,272 @@
+#include <ArduinoJson.h>
+#include <FastLED.h>
+#include <WebSocketsClient.h>
+#include <WiFi.h>
+
+#define NUM_LEDS 865       // Amount of LEDs in strip
+#define PIN_LED 5          // Digital pin of LED (Din)
+#define LED_BRIGHTNESS 200 // brigtness of LEDs, TODO: add control via server
+#define RIPPLE_SPLIT 4     // how much you want to split ripple effect show
+
+#define SERVER_URL "HOST IP" // something like 192.168.1.150
+#define SERVER_PORT 8080     // flask server port
+
+CRGB leds[NUM_LEDS];
+
+const char *ssid = "WIFI SSID";
+const char *password = "WIFI PASSWORD";
+
+// some structs for fun
+struct hsv_color {
+  int hue;
+  int saturation;
+  int value;
+};
+
+struct ripple_params {
+  int hue_gap;
+  int hue_range;
+  int ripple_step;
+};
+
+int counter = 0;
+int led_mode = 0;
+int led_speed = 30;
+int new_year_start = 0;
+bool new_year_start_color = 1;
+long color = 0xFFFFFF;
+unsigned long last_update = 0;
+
+hsv_color color_hsv = {0, 0, 100};
+ripple_params ripple_settings = {0, 0, 0};
+
+WebSocketsClient webSocket;
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
+  switch (type) {
+  case WStype_CONNECTED:
+    Serial.println("Connected to websocket");
+    break;
+
+  case WStype_TEXT:
+    JsonDocument ws_data;
+    DeserializationError error = deserializeJson(ws_data, payload);
+    if (!error) {
+      JsonObject data = ws_data.as<JsonObject>();
+      led_mode = data["led_mode"] | led_mode;
+      led_speed = data["speed"] | led_speed;
+      color = data["custom_color"] | color;
+
+      JsonObject ripple = data["ripple"];
+      if (!ripple.isNull()) {
+        ripple_settings.hue_gap = ripple["hue_gap"] | ripple_settings.hue_gap;
+        ripple_settings.hue_range =
+            ripple["hue_range"] | ripple_settings.hue_range;
+        ripple_settings.ripple_step =
+            ripple["ripple_step"] | ripple_settings.ripple_step;
+      }
+    } else {
+      Serial.printf("JSON error: %s\n", error.c_str());
+    }
+    break;
+  }
+}
+
+void white() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::White;
+  }
+
+  FastLED.show();
+}
+
+void black() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
+  }
+
+  FastLED.show();
+}
+
+void fill_color() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = color;
+  }
+
+  FastLED.show();
+}
+
+bool strab_flag = false; // for non-blocking cycles
+void strab() {
+  if (strab_flag) {
+    for (int j = 0; j < NUM_LEDS; ++j) {
+      leds[j] = CRGB::White;
+    }
+  } else {
+    for (int j = 0; j < NUM_LEDS; ++j) {
+      leds[j] = CRGB::Black;
+    }
+  }
+
+  strab_flag = !strab_flag;
+  FastLED.show();
+}
+
+void new_year(int amount = 10) {
+  CRGB colors[2] = {CRGB::Red, CRGB::White};
+  int cnt = new_year_start++;
+  bool red = new_year_start_color;
+  if (new_year_start == amount) {
+    new_year_start = 0;
+    new_year_start_color = !new_year_start_color;
+  }
+
+  for (int i = 0; i < NUM_LEDS; ++i) {
+    leds[i] = colors[red];
+    if (++cnt == amount) {
+      cnt = 0;
+      red = !red;
+    }
+  }
+  FastLED.show();
+}
+
+int rainbow_offset = 0; // for non-blocking cycles
+void rainbow(int BRIGHTNESS = 255, int SATURATION = 255) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(i - (rainbow_offset * 2), SATURATION, BRIGHTNESS);
+  }
+
+  rainbow_offset = (rainbow_offset + 1) % 255;
+  FastLED.show();
+}
+
+void rippleTick() {
+  int temp;
+  if (RIPPLE_SPLIT) { // ESP32 can't handle this mode with big LED_NUM, so you
+                      // can split it into chunks(looks not that great)
+    for (int k = 0; k < RIPPLE_SPLIT; ++k) {
+      for (int i = k * int(NUM_LEDS / RIPPLE_SPLIT);
+           i < (k + 1) * int(NUM_LEDS / RIPPLE_SPLIT); i++) {
+
+        temp = inoise8(i * ripple_settings.ripple_step, counter);
+        leds[i] = CHSV(
+            map(color_hsv.hue, 0, 360, 0, 255) +
+                map(temp, 20, 60, 0, ripple_settings.hue_gap), // H
+            constrain(map(temp, 20, 60,
+                          map(min(100, color_hsv.saturation +
+                                           ripple_settings.hue_range),
+                              0, 100, 0, 255),
+                          map(max(0, color_hsv.saturation -
+                                         ripple_settings.hue_range),
+                              0, 100, 0, 255)),
+                      0, 255), // S
+            constrain(
+                map(temp, 20, 60,
+                    map(max(0, color_hsv.value - ripple_settings.hue_range), 0,
+                        100, 0, 255),
+                    map(min(100, color_hsv.value + ripple_settings.hue_range),
+                        0, 100, 0, 255)),
+                0, 255) // V
+        );
+        counter += 20;
+      }
+      FastLED.show();
+    }
+  } else {
+    for (int i = 0; i < NUM_LEDS; i++) {
+
+      temp = inoise8(i * ripple_settings.ripple_step, counter);
+      leds[i] = CHSV(
+          map(color_hsv.hue, 0, 360, 0, 255) +
+              map(temp, 20, 60, 0, ripple_settings.hue_gap), // H
+          constrain(
+              map(temp, 20, 60,
+                  map(min(100,
+                          color_hsv.saturation + ripple_settings.hue_range),
+                      0, 100, 0, 255),
+                  map(max(0, color_hsv.saturation - ripple_settings.hue_range),
+                      0, 100, 0, 255)),
+              0, 255), // S
+          constrain(
+              map(temp, 20, 60,
+                  map(max(0, color_hsv.value - ripple_settings.hue_range), 0,
+                      100, 0, 255),
+                  map(min(100, color_hsv.value + ripple_settings.hue_range), 0,
+                      100, 0, 255)),
+              0, 255) // V
+      );
+      counter += 20;
+    }
+    FastLED.show();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  FastLED.addLeds<NEOPIXEL, PIN_LED>(leds, NUM_LEDS);
+  FastLED.setBrightness(LED_BRIGHTNESS);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  webSocket.begin(SERVER_URL, SERVER_PORT, "/ws");
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(3000);
+
+  last_update = millis(); // Using that for non-blocking delays, cause webSocket
+                          // won't work with delay()
+}
+
+void loop() {
+  webSocket.loop();
+
+  if (millis() - last_update > led_speed) { // non-blocking speed delay
+    last_update = millis();
+
+    switch (led_mode) {
+    case 0:
+      black();
+      break;
+    case 1:
+      white();
+      break;
+    case 2:
+      new_year();
+      break;
+    case 3:
+      rainbow();
+      break;
+    case 4:
+      strab();
+      break;
+    case 5:
+      fill_color();
+      break;
+    case 6:
+      rippleTick();
+      break;
+    default:
+      led_mode = 0;
+      break;
+    }
+  }
+
+  // Can be used for debug, so left here
+
+  // Serial.println(led_mode);
+  // delay(200); // For debug
+  // Serial.println(color);
+  // Serial.println(String(color_hsv.hue) + "   " + String(color_hsv.saturation)
+  // + "   " + String(color_hsv.value));
+  // Serial.println(String(ripple_settings.hue_gap) + "   " +
+  // String(ripple_settings.hue_range) + "   " +
+  // String(ripple_settings.ripple_step)); Serial.println("\n\n\n\n\n");
+}
